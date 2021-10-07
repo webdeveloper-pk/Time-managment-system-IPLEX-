553c958539faff3e87ad236d22255e9265a32700
MUHAMMAD ABDULLAH RASHEED <mabdullah.rasheed8@gmail.com>
2021-10-07 12:45:03 +0500

    initial biler-plate+routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+// import { Helmet } from "react-helmet";
+import Home from "./pages/home";
+import ErrorPage from "./pages/errorPage/ErrorPage";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Helmet>
+        <meta charSet="utf-8" />
+        <title>Time Management System</title>
+        <meta name="title" content="Time Management System" />
+      </Helmet> */}
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/404" component={ErrorPage} />
+          <Redirect from="*" to="/404" />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
